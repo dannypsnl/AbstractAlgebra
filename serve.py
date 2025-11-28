56c8fbf0f71f86e6935a666d6a25beaa8fc0397e
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-11-28 22:08:51 +0800

    no need to watch output html now

--- serve.py
+++ serve.py
@@ -1,6 +1,5 @@
 from livereload import Server, shell
 server = Server()
 server.watch('src/**/*.md', shell('make'))
-server.watch('html/*.html', shell('make'))
 server.watch('content/**/*.scrbl', shell('make'))
 server.serve(root='_build', port=5321)
