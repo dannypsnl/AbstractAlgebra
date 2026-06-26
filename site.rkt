7b41da774a731b298ac81bf1c3a2a36e381da168
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2026-06-26 16:08:33 +0800

    new config

---
+++ site.rkt
@@ -0,0 +1,8 @@
+#lang racket/base
+(require scribble/html)
+(provide site)
+
+(define site
+  (hash 'description "Lectures of abstract algebra"
+        'domain "dannypsnl.github.io/AbstractAlgebra"
+        'title "abstract algebra"))
